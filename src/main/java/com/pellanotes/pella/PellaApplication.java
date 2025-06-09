7a5a8f36e0d65fa54b7613157d815e5baedea931
hommym <herbertharthur80@gmail.com>
2025-06-09 06:48:38 +0000

    Initialize Pella project with Spring Boot
    - Added Gradle wrapper files for build management.
    - Created settings.gradle to define the root project name as 'pella'.
    - Implemented main application class PellaApplication with Spring Boot annotations.
    - Configured application properties for database connection and server settings.
    - Added test class PellaApplicationTests to verify application context loading.

---
+++ src/main/java/com/pellanotes/pella/PellaApplication.java
@@ -0,0 +1,17 @@
+package com.pellanotes.pella;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication  
+  
+public class PellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pellanotes/pella/PellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pellanotes.pella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
