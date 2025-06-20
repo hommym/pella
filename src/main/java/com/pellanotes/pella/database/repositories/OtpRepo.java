2c38e56d9b96a7e1c9e165c12586bb2efb32d84c
hommym <herbertharthur80@gmail.com>
2025-06-20 09:44:23 +0000

    Add OtpRepo interface for OTP entity management

---
+++ src/main/java/com/pellanotes/pella/database/repositories/OtpRepo.java
@@ -0,0 +1,12 @@
+package com.pellanotes.pella.database.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pellanotes.pella.database.models.Otp;
+
+public interface OtpRepo extends JpaRepository<Otp, Long>{
+    
+
+    
+
+}
