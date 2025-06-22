018718ad1c3d917f456e579aab2b60db2832dfe3
hommym <herbertharthur80@gmail.com>
2025-06-22 19:36:20 +0000

    Refactor AuthService to include EmailService and enhance signup method with email notifications

--- src/main/java/com/pellanotes/pella/features/auth/AuthService.java
+++ src/main/java/com/pellanotes/pella/features/auth/AuthService.java
@@ -2,6 +2,8 @@ package com.pellanotes.pella.features.auth;
 
 import org.springframework.stereotype.Service;
 
+import com.pellanotes.pella.common.exceptions.ResourceConflict;
+import com.pellanotes.pella.common.utils.EmailService;
 import com.pellanotes.pella.common.utils.PasswordService;
 import com.pellanotes.pella.database.models.Otp;
 import com.pellanotes.pella.database.models.User;
@@ -20,27 +22,36 @@ public class AuthService {
     private final UserRepo userRepo;
     private final OtpRepo otpRepo;
     private final PasswordService passwdService;
+    private final EmailService   emailService;
 
 
-    public AuthService(UserRepo userRepo,OtpRepo otpRepo,PasswordService passwdService){
+    public AuthService(UserRepo userRepo,OtpRepo otpRepo,PasswordService passwdService,EmailService   emailService){
         this.userRepo=userRepo;
         this.otpRepo=otpRepo;
         this.passwdService=passwdService;
+        this.emailService=emailService;
     }
 
     @Transactional
     SignUpResponse signup(SignUpDto dto){
+
+        // check if email and username exist
+        if(userRepo.checkEmail(dto.email).isPresent())throw new ResourceConflict("User with this email already exist");
+        else if (userRepo.checkUserName(dto.username).isPresent())throw new ResourceConflict("User with this username already exist");
+
         User user= new User(dto.fullName,dto.email,passwdService.encryptPasswd(dto.password),dto.username);
         user= userRepo.save(user);
         // set up otp 
         Otp otp= new Otp(user);
         otp= otpRepo.save(otp);
 
+        // sending congrats email
+        emailService.sendSignUpEmail(dto.email, dto.username);
+
         //send otp to user
+        emailService.sendOtpEmail(dto.email, dto.username, otp.getOtpCode());
 
-
-
-        return new SignUpResponse("Account creation successful, verify account to login.");
+        return new SignUpResponse("Account creation successful, verify account to login");
     }
 
     
