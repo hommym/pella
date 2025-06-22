6e6a3487fbe82f79b66cf5f10c36b8eae4fc3005
hommym <herbertharthur80@gmail.com>
2025-06-22 19:35:28 +0000

    Add ResourceConflict and ResourceNotFound exception classes for improved error handling

---
+++ src/main/java/com/pellanotes/pella/common/exceptions/ResourceConflict.java
@@ -0,0 +1,8 @@
+package com.pellanotes.pella.common.exceptions;
+
+public class ResourceConflict extends  RuntimeException{
+    
+   public  ResourceConflict(String message){
+    super(message);
+    }
+}
