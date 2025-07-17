05ad7106d9dbfa817bc34ab5c3a15eeb1e4f42b1
hommym <herbertharthur80@gmail.com>
2025-07-17 05:39:42 +0000

    Refactor Note class: add access modifiers and annotations for title and content fields

--- src/main/java/com/pellanotes/pella/database/models/Note.java
+++ src/main/java/com/pellanotes/pella/database/models/Note.java
@@ -10,19 +10,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name="note")
+@Table(name="note",uniqueConstraints={@UniqueConstraint(columnNames={"note_book_id","title"})})
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    String title;
+   
+    private String title;
 
-    String content; 
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String content; 
     
     @Column(name="note_book_id",insertable=false,updatable=false)
     private Long noteBookId;
@@ -49,9 +54,28 @@ public class Note {
 
 
 
-    Long getId(){
+    public   Long getId(){
         return this.id;
     }
 
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+
+    public Long getNoteBookId(){
+        return this.noteBookId;
+    }
+
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public void setContent(String updatedContent){
+        this.content=updatedContent;
+    }
 
 }
