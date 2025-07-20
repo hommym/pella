ec255aa6b70c0d54fee5946717bcbb54a1479381
hommym <herbertharthur80@gmail.com>
2025-07-20 18:43:17 +0000

    Added a component for adding references to NoteResponse

--- src/main/java/com/pellanotes/pella/features/note/dtos/NoteResponse.java
+++ src/main/java/com/pellanotes/pella/features/note/dtos/NoteResponse.java
@@ -1,6 +1,10 @@
 package com.pellanotes.pella.features.note.dtos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.pellanotes.pella.database.models.Note;
+import com.pellanotes.pella.database.models.NoteReference;
 
 public class NoteResponse {
     
@@ -12,6 +16,8 @@ public class NoteResponse {
 
     public Long noteBookId;
 
+    public List<NoteRefResponse> noteReferences=new ArrayList<>();
+
 
     public NoteResponse(Note note){
 
@@ -19,6 +25,10 @@ public class NoteResponse {
         this.title=note.getTitle();
         this.content=note.getContent();
         this.noteBookId=note.getNoteBookId();
+
+        for(NoteReference ref:note.getReferences()){
+            this.noteReferences.add(new NoteRefResponse(ref));
+        }
     }
 
 
